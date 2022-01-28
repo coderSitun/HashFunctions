15df9e39220954b0237b09112e9d822238f741eb
coderSitun <ksaswat007@gmail.com>
2022-01-28 22:01:20 +0530

    Added partial defination for Sha160::fillWords()

--- src/sha160/sha160.cpp
+++ src/sha160/sha160.cpp
@@ -13,6 +13,40 @@ void Sha160::fillCharInWord(const uint32 wordIndex, const char theChar){
     words[wordIndex] |= theChar
 }
 
+void Sha160::fillWords(std::string message, const uin32 offset){
+    uint32 wordIndex = -1;
+    uint32 curIndex  = 0;
+    uint32 curLength = message.length() - offset;
+    curLength = (curLength < setSize)?curLength:setSize;
+    uint32 wordsForMessage = curLength >> charsPerWordBitLength;
+
+    while(wordIndex < wordsForMessage){
+        words[wordIndex] = 0;
+        for(index = 0; index < charsPerWord; ++index, ++curIndex){
+            fillCharInWord(wordIndex, message[offset + curIndex]);
+        }
+        ++wordIndex;
+    }
+
+    words[wordIndex] = 0;
+    while(curIndex < curLength){
+        fillCharInWord(wordIndex, message[offset + curIndex]);
+    }
+    curIndex = curLength & charsPerWordMinusOne;
+    if(curIndex < charsPerWord){
+        fillCharInWord(wordIndex, 0x80);
+        ++curIndex;
+    }
+    while(curIndex < charsPerWord){
+        fillCharInWord(wordIndex, 0x00);
+        ++curIndex;
+    }
+    ++wordIndex;
+
+    while(wordIndex < wordsPerSet)
+        words[wordIndex++] = 0x00000000;
+}
+
 bool Sha160::checkValidity(){
     return true;
 }
