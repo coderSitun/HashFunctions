f0ffad01e139380f12ef4ead86ff2d0375da4637
coderSitun <ksaswat007@gmail.com>
2022-01-28 21:59:55 +0530

    Added Sha160::fillCharInWord defination

--- src/sha160/sha160.cpp
+++ src/sha160/sha160.cpp
@@ -8,6 +8,11 @@ Sha160::Sha160(){
     words = new uint32[totalWords];
 }
 
+void Sha160::fillCharInWord(const uint32 wordIndex, const char theChar){
+    words[wordIndex] <<= charBitLength;
+    words[wordIndex] |= theChar
+}
+
 bool Sha160::checkValidity(){
     return true;
 }
