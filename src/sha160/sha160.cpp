c747afcc3f89295cd0b3254916e81ea78db7039c
coderSitun <ksaswat007@gmail.com>
2022-01-28 20:51:05 +0530

    Added constant Sha160::totalWords + private member Sha160::*words + constructor for Sha160

--- src/sha160/sha160.cpp
+++ src/sha160/sha160.cpp
@@ -2,6 +2,11 @@
 
 const Hash::uint32 Sha160::setSize     = 512;
 const Hash::uint32 Sha160::wordsPerSet = 16;
+const Hash::uint32 Sha160::totalWords  = 80;
+
+Sha160::Sha160(){
+    words = new uint32[totalWords];
+}
 
 bool Sha160::checkValidity(){
     return true;
--- src/sha160/sha160.h
+++ src/sha160/sha160.h
@@ -7,7 +7,10 @@ class Sha160:public Hash{
 private:
     static const uint32 setSize;
     static const uint32 wordsPerSet;
+    static const uint32 totalWords;
+    uint32 *words;
 public:
+    Sha160();
     bool checkValidity();
     std::string getHash(std::string message);
 };
