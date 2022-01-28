44e159cb527dba94e0c83e8c9972a311ee8977ff
coderSitun <ksaswat007@gmail.com>
2022-01-28 21:52:06 +0530

    Added Prototypes Sha160::fillCharInWord() + Sha160::fillWords()

--- src/sha160/sha160.h
+++ src/sha160/sha160.h
@@ -9,6 +9,8 @@ private:
     static const uint32 wordsPerSet;
     static const uint32 totalWords;
     uint32 *words;
+    void fillCharInWord(const uint32 wordIndex, const char theChar);
+    void fillWords(std::string message, const uin32 offset);
 public:
     Sha160();
     bool checkValidity();
